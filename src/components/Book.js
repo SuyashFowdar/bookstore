6b1fd6b21aa53d229cb597327ed7a6bb5c704402
Suyash <suyash.krishna9@gmail>
2021-03-31 18:24:55 +0400

    Connect removeBook function

--- src/components/Book.js
+++ src/components/Book.js
@@ -1,12 +1,20 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import { removeBook } from '../actions/index';
 
-const Book = ({ title, category, id }) => (
+const Book = ({
+  title,
+  category,
+  id,
+  deleteBook,
+}) => (
   <>
     <tr>
       <td>{id}</td>
       <td>{title}</td>
       <td>{category}</td>
+      <td><button type="button" onClick={() => deleteBook({ id })}>Remove</button></td>
     </tr>
   </>
 );
@@ -15,5 +23,12 @@ Book.propTypes = {
   id: PropTypes.number.isRequired,
   title: PropTypes.string.isRequired,
   category: PropTypes.string.isRequired,
+  deleteBook: PropTypes.func.isRequired,
 };
-export default Book;
+
+const mapDispatchToProps = dispatch => ({
+  deleteBook: book => {
+    dispatch(removeBook(book));
+  },
+});
+export default connect(null, mapDispatchToProps)(Book);
