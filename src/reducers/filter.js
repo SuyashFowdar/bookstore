cf0e4878b3ec786ea2ac7f53e4eebad3d4b70eca
Suyash <suyash.krishna9@gmail>
2021-04-01 17:05:32 +0400

    Add filter reducer

---
+++ src/reducers/filter.js
@@ -0,0 +1,15 @@
+import { CHANGE_FILTER } from '../actions/index';
+
+const filterReducer = (state = { filter: 'All', filteredBooks: [] }, action) => {
+  switch (action.type) {
+    case CHANGE_FILTER:
+      return {
+        filter: action.filter,
+        filteredBooks: action.filter === 'All' ? action.books : action.books.filter((el) => el.category === action.filter),
+      };
+    default:
+      return state;
+  }
+};
+
+export default filterReducer;
