ca2156433848e236dc382eee83906146b188fa76
Suyash <suyash.krishna9@gmail>
2021-04-02 17:21:18 +0400

    Refactor code

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,19 +1,15 @@
-export const CREATE_BOOK = 'CREATE_BOOK';
-export const REMOVE_BOOK = 'REMOVE_BOOK';
-export const CHANGE_FILTER = 'CHANGE_FILTER';
-
 export const createBook = (book) => ({
-  type: CREATE_BOOK,
+  type: 'CREATE_BOOK',
   book,
 });
 
 export const removeBook = (book) => ({
-  type: REMOVE_BOOK,
+  type: 'REMOVE_BOOK',
   book,
 });
 
 export const changeFilter = (books, filter) => ({
-  type: CHANGE_FILTER,
+  type: 'CHANGE_FILTER',
   books,
   filter,
 });
